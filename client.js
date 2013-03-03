f755f755b1a8345585dd05502b5db23ad44c5e15
Marcel Klehr <mklehr@gmx.net>
2013-03-03 21:31:30 +0100

    Harden everything
    -Multiple documents
    -More like a client-side lib
    -Make it Disconnect robust

--- client.js
+++ client.js
@@ -1,9 +1,22 @@
 ﻿var CollabClient = require('./lib/CollabClient')
   , Connection = require('./lib/Connection')
   , Editor = require('./lib/Editor')
+  , Emitter = require('emitter')
 
 // KICK ASS!
-var socket = io.connect('http://localhost')
-  , connection = new Connection(socket)
-  , editor = new Editor($('.content'))
-  , adapter = new CollabClient(editor, connection)
+exports.connect = function(options) {
+  var options =
+  { element: options.element || null
+  , server: options.server || 'http://localhost'
+  , document: options.document || null
+  }
+  for(var prop in options) if (!options[prop]) throw new Error('warp: "'+prop+'" option is not set')
+  
+  var socket = io.connect(options.server, { 'reconnection limit': 5000, 'max reconnection attempts': Infinity }).of('warp')
+ 
+  var connection = new Connection(socket)
+    , editor = new Editor(options.element)
+  var client = new CollabClient(options.document, editor, connection)
+  
+  return connection
+}
--- server.js
+++ server.js
@@ -10,16 +10,24 @@
 //app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.static(__dirname))
-app.all('/', function(req, res){
+app.all('*', function(req, res){
   res.sendfile('index.html');
 });
-server.listen(80);
+server.listen(9009);
 
-var doc = new Document('Hello World')
+var initialDocument = '<p>A minimalist writing zone, where you can block out all distractions and get to what\'s important. The writing!</p><p>As if that\'s not enough, you can easily share this zone with anyone and write together. In real-time.</p><p>To get started, all you need to do is delete this text (seriously, just highlight it and hit delete), and fill the page with your own fantastic words.</p><p>You can use <b>bold</b>, <i>italics</i>,<b><i>both</i></b> and <a href="http://zenpen.io">urls</a> just by highlighting the text and selecting them from the tiny options box that appears above it.</p><blockquote>Quotes are easy to add too!</blockquote><p>Zenpen is what you see, by Tim Holman<br>Warp is what you feel, by Marcel Klehr</p><p>Happy editing! &lt;3</p>'
+
+var docs = {}
 
 
 // S.IO
 
-io.on('connection', function(socket){
-  doc.addConnection(new Connection(socket))
+io.of('warp').on('connection', function(socket){
+  console.log('new connection: '+socket.id)
+  var conn = new Connection(socket)
+  conn.once('ready', function(data) {
+    if(!docs[data.document]) docs[data.document] = new Document(initialDocument)
+    docs[data.document].addConnection(conn)
+    conn.emit('ready', data)
+  })
 })
