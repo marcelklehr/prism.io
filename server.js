84301582f8b3d3d927b2a400bebefe880d368b78
Marcel Klehr <mklehr@gmx.net>
2013-03-03 22:45:22 +0100

    Update initial doc.

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.all('*', function(req, res){
 });
 server.listen(9009);
 
-var initialDocument = '<p>A minimalist writing zone, where you can block out all distractions and get to what\'s important. The writing!</p><p>As if that\'s not enough, you can easily share this zone with anyone and write together. In real-time.</p><p>To get started, all you need to do is delete this text (seriously, just highlight it and hit delete), and fill the page with your own fantastic words.</p><p>You can use <b>bold</b>, <i>italics</i>,<b><i>both</i></b> and <a href="http://zenpen.io">urls</a> just by highlighting the text and selecting them from the tiny options box that appears above it.</p><blockquote>Quotes are easy to add too!</blockquote><p>Zenpen is what you see, by Tim Holman<br>Warp is what you feel, by Marcel Klehr</p><p>Happy editing! &lt;3</p>'
+var initialDocument = '<p>A minimalist writing zone, where you can block out all distractions and get to what\'s important. The writing!</p><p>To get started, all you need to do is delete this text (seriously, just highlight it and hit delete), and fill the page with your own fantastic words.</p><p>You can use <b>bold</b>, <i>italics</i>,<b><i>both</i></b> and <a href="http://zenpen.io">urls</a> just by highlighting the text and selecting them from the tiny options box that appears above it.</p><blockquote>Quotes are easy to add too!</blockquote><p>And if you want joined-up writing, just share this url with anyone in the world and you can write together. Seriously!</p><p>Zenpen is what you see, by Tim Holman<br>Warp is what you feel, by Marcel Klehr</p><p>Happy typing! &lt;3</p>'
 
 var docs = {}
 
